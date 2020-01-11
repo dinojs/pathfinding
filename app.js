e789f4548625fe6834f63a82a721513a99f148c7
Dino <dinohossain1@gmail.com>
2020-01-11 21:26:39 +0000

    Added nodes for New York

--- app.js
+++ app.js
@@ -3,5 +3,28 @@ import {AmbientLight, PointLight, LightingEffect} from '@deck.gl/core'
 import {HeatmapLayer, IconLayer, PolygonLayer, TripsLayer} from '@deck.gl/layers';
 import mapboxgl from 'mapbox-gl'
 
-const DATA = 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson'
+const DATA = {
+    BULDINGS: 
+        'https://raw.githubusercontent.com/dinohossain/pathfinding/master/data/buildings.json',
+    NODES: 
+        'https://raw.githubusercontent.com/dinohossain/pathfinding/master/data/roads.json',
+    TRIPS:
+        'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/trips/trips-v7.json'
+}
 
+layers: {
+    new TripsLayer({
+        id: 'trips',
+        data: trips,
+        getPath: d => d.path,
+        getTimestamps: d => d.timestamps,
+        getColor: d => (d.vendor === 0 ? theme.trailColor0 : theme.trailColor1),
+        opacity: 0.3,
+        widthMinPixels: 2,
+        rounded: true,
+        trailLength,
+        currentTime: this.state.time,
+
+        shadowEnabled: false
+      })
+}
