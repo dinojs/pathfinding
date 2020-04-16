7a612e11055351fd46fcd898129a18bf59fb4166
Dino <dinohossain1@gmail.com>
2020-04-17 00:45:24 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.54243c47d7cb7a7712961445daab6a4c.js"
+  "/precache-manifest.e7160f38cbf776661d516b31d4e5ac42.js"
 );
 
 self.addEventListener('message', (event) => {
